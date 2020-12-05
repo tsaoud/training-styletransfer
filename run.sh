e2ca51f8c34be1dd3d113e4ddc677fafd4cc517b
tsaoud <61511495+tsaoud@users.noreply.github.com>
2020-12-05 14:24:23 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ python style.py --style /storage/datasets/styles/picasso1.jpg \
   --checkpoint-dir /storage/checkpoints \
   --model-dir /storage/checkpoints \
   --test /storage/datasets/styles/TarekHeadshot.jpeg \
-  --test-dir /storage/samples \
+  --test-dir /storage/datasets/styles/ \
   --content-weight 1.5e1 \
   --checkpoint-iterations 1000 \
   --batch-size 20
